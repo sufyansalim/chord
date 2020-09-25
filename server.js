e7aa3ffd2d83720b387fbf0ac5fb991ff612ac84
Sufyan Saleem <40366969+sufyansalim@users.noreply.github.com>
2020-09-25 19:35:51 +0500

    get server address

--- server.js
+++ server.js
@@ -85,4 +85,4 @@ const server = http.createServer((req,res)=>{
 
 const PORT = 5000;
 
-server.listen(PORT,()=>console.log(`Server running on port ${PORT}`))
+server.listen(PORT,()=>console.log(`Server running on port ${PORT}`,server.address()))
